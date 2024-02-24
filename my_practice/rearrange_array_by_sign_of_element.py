f4af601725baea7ac6503eaebad884da03b5cbe8
Asgef <asgefes1@gmail.com>
2024-02-24 22:23:13 +0300

    Add problem: "Rearrange Array by Sign"

---
+++ my_practice/rearrange_array_by_sign_of_element.py
@@ -0,0 +1,43 @@
+# Problem: Rearrange Array by Sign
+#
+# Given an array consisting of positive (n > 0) and negative (n < 0) numbers,
+# rearrange the array in such a way that:
+# - each consecutive pair has opposite signs;
+# - the sequence of numbers with the same signs remains unchanged after
+#   rearrangement;
+# - the first element is always positive.
+#
+# Example:
+# [-4, 3, 1, -2, 1, -3] -> [3, -4, 1, -2, 1, -3]
+# [1, 2, 3, -3, -1, -2] -> [1, -3, 2, -1, 3, -2]
+
+
+# Задача: Перестановка массива по знаку элемента
+#
+# Дан массив, который состоит из положительных (n > 0) и отрицательных (n < 0)
+# чисел. Необходимо переставить массив таким образом, чтобы:
+# - каждая последовательная пара имела противоположные знаки;
+# - последовательность чисел с одинаковыми знаками оставалась неизменной после
+#   перестановки;
+# - первый элемент всегда был положительным.
+#
+# Пример:
+# [-4, 3, 1, -2, 1, -3] -> [3, -4, 1, -2, 1, -3]
+# [1, 2, 3, -3, -1, -2] -> [1, -3, 2, -1, 3, -2]
+
+
+def rearrange_array(nums):
+    positive_idx = 0
+    negative_idx = 1
+    result = [None, None, None, None, None, None]
+
+    for elem in nums:
+        if elem > 0:
+            result[positive_idx] = elem
+            positive_idx += 2
+
+        elif elem < 0:
+            result[negative_idx] = elem
+            negative_idx += 2
+
+    return result
---
+++ tests/test_rearrange_array_by_sign_of_element.py
@@ -0,0 +1,7 @@
+from my_practice.rearrange_array_by_sign_of_element import rearrange_array
+
+
+def test_rearrange_array_by_sign():
+    assert rearrange_array([-4, 3, 1, -2, 1, -3]) == [3, -4, 1, -2, 1, -3]
+    assert rearrange_array([1, 2, 3, -3, -1, -2]) == [1, -3, 2, -1, 3, -2]
+    assert rearrange_array([1, -1, 2, -2, 3, -3]) == [1, -1, 2, -2, 3, -3]
