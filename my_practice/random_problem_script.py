f57c978d3ebb2bfb78a498fbb736d8d811d5a8cb
Asgef <asgefes1@gmail.com>
2024-08-08 16:30:03 +0300

    Fix lint

--- my_practice/random_problem_script.py
+++ my_practice/random_problem_script.py
@@ -19,6 +19,7 @@ def save_data(file_path, data):
     with open(file_path, 'w+') as file:
         json.dump(data, file)
 
+
 def get_files(dir):
     """Get list of files in the directory and the latest file."""
     list_files = [
@@ -31,14 +32,18 @@ def get_files(dir):
            ]
     ]
 
-    latest_file = max(list_files, key=lambda x: os.path.getctime(os.path.join(dir, x)))
+    latest_file = max(
+        list_files, key=lambda x: os.path.getctime(os.path.join(dir, x))
+    )
     return list_files, latest_file
 
 
 def update_intervals(file_data, file_name, success):
     """Update the intervals for the given file based on success."""
     if file_name not in file_data:
-        file_data[file_name] = {'interval': 1, 'last_reviewed': str(datetime.now())}
+        file_data[file_name] = {
+            'interval': 1, 'last_reviewed': str(datetime.now())
+        }
     else:
         interval = file_data[file_name]['interval']
         if success:
@@ -52,11 +57,17 @@ def update_intervals(file_data, file_name, success):
 def select_file_for_review(file_data):
     """Select a file for review based on the intervals."""
     now = datetime.now()
-    due_files = [file for file, data in file_data.items() if datetime.fromisoformat(data['last_reviewed']) + timedelta(days=data['interval']) <= now]
+    due_files = [
+        file for file, data in file_data.items()
+        if datetime.fromisoformat(data['last_reviewed']) + timedelta(
+            days=data['interval']
+        ) <= now
+    ]
     if due_files:
         return random.choice(due_files)
     else:
         return random.choice(list(file_data.keys()))
+
 
 def get_enquiry():
     enqr = input('Успешно повторил? "yes/no": ')
@@ -75,7 +86,9 @@ def get_enquiry():
 def main():
     # Путь к сохраненным данным
     current_dir = os.path.dirname(__file__)
-    file_path = os.path.join(current_dir, '../tests/fixtures/random_problem/random_problem')
+    file_path = os.path.join(
+        current_dir, '../tests/fixtures/random_problem/random_problem'
+    )
 
     # Считывание данных или создание нового файла
     file_data = load_data(file_path)
@@ -87,7 +100,9 @@ def main():
     # Добавляем новые файлы в данные
     for file in list_files:
         if file not in file_data:
-            file_data[file] = {'interval': 1, 'last_reviewed': str(datetime.now())}
+            file_data[file] = {
+                'interval': 1, 'last_reviewed': str(datetime.now())
+            }
 
     # Выбор файла для повторения
     file_for_review = select_file_for_review(file_data)
@@ -95,7 +110,7 @@ def main():
 
     # Обновление интервалов (например, пользователь успешно повторил материал)
 
-    success = get_enquiry()  # Здесь можно добавить логику для оценки успеха
+    success = get_enquiry()
     file_data = update_intervals(file_data, file_for_review, success)
 
     # Сохранение обновленных данных
