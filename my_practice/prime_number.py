a7b3604ba74bcdf2c588171d804e293e15563444
Asgef <asgefes1@gmail.com>
2024-09-03 15:13:17 +0300

    Repeat the exercise "prime_number.py"

--- my_practice/prime_number.py
+++ my_practice/prime_number.py
@@ -11,11 +11,10 @@
 # is_prime(4) Возвращает False
 
 
-def is_prime(num):
+def is_prime(num: int) -> bool:
     if num < 2:
         return False
-    root_num = int(num ** (1/2))
-    for i in range(2, root_num + 1):
+    for i in range(2, int(num ** 1/2) + 1):
         if num % i == 0:
             return False
     return True
